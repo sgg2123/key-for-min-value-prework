f515b9b292bf4983bcf0ddbe469af0ac1f102990
sgg2123 <sgg2123@columbia.edu>
2018-04-20 20:41:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,12 @@ def key_for_min_value(name_hash)
   else
     min_key = name_hash.first[0]
     min_val = name_hash.first[1]
-    
-
+    name_hash.each do |k, v|
+      if val < min_val
+        min_val = v
+        min_key = k
+      end
+    end
+    min_key
+  end
 end
