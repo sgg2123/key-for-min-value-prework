139b0d0b7ed3fcea3ec419e26d97ef075114ae5b
sgg2123 <sgg2123@columbia.edu>
2018-04-20 20:34:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  if name_hash == {}
+    nil
+  else
+    min_val = name_hash[0]
+    min_key = name_hash
 
 end
